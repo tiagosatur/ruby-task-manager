c3e8dac0bcad4c6abde59f06304668e9e50f7889
Tiago Satur <tiagosatur@gmail.com>
2025-07-07 00:27:34 -0300

    Set default status pending in task model and add validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :status, inclusion: { in: %w[pending in_progress done] }
+
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= "pending"
+  end
 end
